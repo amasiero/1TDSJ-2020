2ac501a11ddb67820c6affd814bb68905b437e42
Andrey Masiero <andreymasiero@gmail.com>
2020-08-28 11:51:53 -0300

    Exemplos do LocalDate

--- Collection/src/br/com/fiap/collection/TesteCategoria.java
+++ Collection/src/br/com/fiap/collection/TesteCategoria.java
@@ -1,8 +1,21 @@
 package br.com.fiap.collection;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 enum Categoria {
-	APARTAMENTO, // 0
-	VIP; // 1
+	APARTAMENTO("Apartamento"), // 0
+	VIP("Vip"); // 1
+	
+	private String nome;
+	
+	private Categoria(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
 }
 
 public class TesteCategoria {
@@ -11,10 +24,22 @@ public class TesteCategoria {
 		Categoria a = Categoria.APARTAMENTO;
 		
 		if(a == Categoria.VIP) {
-			System.out.println(Categoria.VIP);
+			System.out.println(Categoria.VIP.getNome());
 		} else {
-			System.out.println(Categoria.APARTAMENTO);
+			System.out.println(Categoria.APARTAMENTO.getNome());
 		}
+		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		LocalDate data = LocalDate.now();
+		LocalDate amanha = LocalDate.of(2020, 8, 29);
+		
+		LocalDate doisDiasAFrente = data.plusDays(2);
+		
+		System.out.println(formatter.format(data));
+		System.out.println(formatter.format(doisDiasAFrente));
+		System.out.println(formatter.format(amanha));
+		System.out.println(amanha.isBefore(data));
+		System.out.println(doisDiasAFrente.isAfter(amanha));
 	}
 	
 }
